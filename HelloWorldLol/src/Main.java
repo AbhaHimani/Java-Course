8c6999260ea44ae8ed81d74d2704862da0d6a40f
Abha Himani <abhahimani2002@gmail.com>
2022-07-29 15:03:57 +0530

    Day1 w Java lol

---
+++ HelloWorldLol/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+      System.out.println("Hello Tim");
+      int num=(5+10) + (2*10);
+      int secondNum= 12;
+      int thirdNum=num*2;
+      int myTot= num+secondNum+thirdNum;
+        System.out.println(myTot);
+        System.out.println(num);
+        int myLastOne= 1000-myTot;
+        System.out.println(myLastOne);
+    }
+}
